601d93194fa073c7fcb1b1f50c2d427f348dda25
Amir Sdeki <sdeki.amir@gmail.com>
2022-04-03 12:11:49 +0430

    WIP: fix build errors

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import About from "../components/about/About";
 import Hero from "../components/Hero/Hero";
-import Service from "../components/service/service";
+import Service from "../components/service/Service";
 
 export default function Home() {
   return (
